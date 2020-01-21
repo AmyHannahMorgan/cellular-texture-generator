8c076f90b7a58b80d9c2d9c2f5bd40a5a5954e2c
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-21 20:54:33 +0000

    + code to spawn first poisson disc

--- main.js
+++ main.js
@@ -22,11 +22,27 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
 const points = [];
+const poissons = [];
 
-for(let x = 0; x < canvas.height; x++) {
+const stepDelay = 1;
+const startingRadius = 2;
+const radiusStep = 1
+
+for(let x = 0; x < canvas.width; x++) {
     let ar = [];
     for(let y = 0; y < canvas.height; y++) {
         ar.push(new Point(x, y, 0));
+    }
+}
+
+let step = 0;
+
+while(!checkVisitedPercentage(points)) {
+    if(step === stepDelay) {
+        
+    }
+    else if(poissons.length === 0) {
+        poissons.push(new Psudopoisson(RNG(0, canvas.width), RNG(0, canvas.height), 0, startingRadius, radiusStep));
     }
 }
 
