87223dcf7e26b92317e1ee7fd05dc79affd343ac
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-21 18:43:00 +0000

    + basics of Point and Psudopoisson classes

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+class Psudopoisson {
+    constructor(x, y, z, radius, step) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.radius = radius
+        this.step = step;
+    }
+}
+
+class Point {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.visited = false;
+        this.value = 0;
+    }
+}
+
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
